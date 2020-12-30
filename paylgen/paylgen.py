a0841849d6eacaad1ae5d0e0b4437be4ad141f9e
modernpacifist <modernpacifist@gmail.com>
2020-12-30 18:07:21 +0300

    moved to fire instead of argparse

---
+++ paylgen/paylgen.py
@@ -0,0 +1,12 @@
+#!/bin/env python3.9
+
+from fire import Fire
+from PayloadFileGenerator import PayloadFileGenerator
+
+
+def main():
+    Fire(PayloadFileGenerator)
+
+
+if __name__ == '__main__':
+    main()
