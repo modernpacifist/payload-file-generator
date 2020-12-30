b669418b53f9c63e8bcef16f5a6ad4b096d06f73
modernpacifist <modernpacifist@gmail.com>
2020-12-30 05:15:05 +0300

    first minimalistic version

--- pshellgen.py
+++ pshellgen.py
@@ -1,15 +1,6 @@
 #!/bin/env python3.8
 import sys
-
-
-# this is probably useless
-class Translator:
-    def __init__(self, normal_string):
-        self._normal_string = normal_string
-        self.bytestring = None
-
-    def translate(self):
-        self.bytestring = bytes(self._normal_string, "utf-8")
+import argparse
 
 
 class PayloadFileGenerator:
@@ -18,40 +9,36 @@ class PayloadFileGenerator:
         self.nop_slide_length = nop_slide_length
         self.payload = payload
         self.nop_slide = None
+        self._generate_nop_slide()
 
     def _generate_nop_slide(self):
         self.nop_slide = b"\x90" * self.nop_slide_length
 
-    def save_to_file(self):
-        self._generate_nop_slide()
+    def generate_payload_file(self):
         with open(f"{self.filename}.payload", "wb") as f:
             f.write(self.nop_slide + bytes(self.payload, "utf-8"))
 
 
-# def parse_args():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("--payload", "-p", dest="payload_string", type=str, required=True,
-    #                   # help="specify payload string in hex for translation")
-    # parser.add_argument("--out", "-o", dest="filename", type=str, required=False,
-    #                   # help="specify file to save result to")
-    # return parser.parse_args()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--outfile", "-o", dest="outfile", type=str, required=True,
+                        help="file save payload to")
+    parser.add_argument("--length", "-l", dest="nop_slide_length", type=int, required=True,
+                        help="length of the NOP slide")
+    parser.add_argument("--payload", "-p", dest="payload", type=str, required=True,
+                        help="your custom payload")
+    return parser.parse_args()
 
 
 def main():
-    # args = parse_args()
-    # original_string = args.payload_string
-    # filename = args.filename
+    args = parse_args()
 
-    if len(sys.argv) != 4:
-        print("error in args")
-        sys.exit(1)
-
-    filename = sys.argv[1]
-    length = int(sys.argv[2])
-    payload = sys.argv[3]
+    filename = args.outfile
+    length = args.nop_slide_length
+    payload = args.payload
 
     fileGen = PayloadFileGenerator(filename, length, payload)
-    fileGen.save_to_file()
+    fileGen.generate_payload_file()
 
 
 if __name__ == '__main__':
