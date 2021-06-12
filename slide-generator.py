3f628259e61bc0f13e1f19020fd63f323af52bf0
modernpacifist <modernpacifist@gmail.com>
2021-06-12 15:09:12 +0300

    save to file/screen from this point

--- slide-generator.py
+++ slide-generator.py
@@ -5,21 +5,21 @@ from argparse import ArgumentParser as ap
 
 class PayloadGenerator:
     def __init__(self, length: int, outfile: str):
-        self._length: int = length
-        self._nop_ramp: str = None
-        self._outfile: str = outfile
+        self._length = length
+        self._nop_slide = None
+        self._outfile = outfile
 
-        self.generate_nop_ramp()
+        self.generate_nop_slide()
 
-    def generate_nop_ramp(self):
-        self._nop_ramp = repr(b'\x90' * self._length)[2:-1]
+    def generate_nop_slide(self):
+        self._nop_slide = b'\x90' * self._length
 
-        print(self._nop_ramp)
+        print(repr(self._nop_slide)[2:-1])
 
-    # def _get_ramp(self):
-        # if self._outfile:
-            # with open(self._outfile) as f:
-                # f.write(self._nop_ramp)
+    def _save_to_file(self):
+        if self._outfile:
+            with open(self._outfile) as f:
+                f.write(self._nop_slide)
 
 
 def get_args():
@@ -28,6 +28,8 @@ def get_args():
                         help="length of the nop slide")
     parser.add_argument("-o", "--outfile", dest="outfile", type=str, required=False,
                         help="file to dump payload to")
+    parser.add_argument("-p", "--payload", dest="payload", type=str, required=False,
+                        help="your custom payload in bytes, will be inserted after nop slide")
     return parser.parse_args()
 
 
