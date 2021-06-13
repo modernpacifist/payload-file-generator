d731093298f74017492fdf1f59043ebd9ac427d2
modernpacifist <modernpacifist@gmail.com>
2021-06-13 18:42:47 +0300

    first working ver

--- payload-generator.py
+++ payload-generator.py
@@ -12,27 +12,27 @@ class NopSlideGenerator:
 
         self._generate_slide()
 
-    def _generate_slide(self) -> bytes:
-        self._nop_slide = b'\x90' * self._length
+    def _generate_slide(self) -> str:
+        self._nop_slide = "\\x90" * self._length
 
     def get_slide(self):
         return self._nop_slide
 
 
 class PayloadInserter:
-    def __init__(self, nop_slide: bytes, payload: str):
+    def __init__(self, nop_slide: str, payload: str, outfile: str):
         self._nop_slide = nop_slide
         self._payload = payload
+        self._outfile = outfile
         self._comb_res = None
 
         self._insert_shellcode()
-        self.print_nop()
 
     def _insert_shellcode(self):
         self._comb_res = self._nop_slide + self._payload
 
-    def print_nop(self):
-        print(repr(self._comb_res)[2:-1])
+    def get_combined_res(self):
+        return self._comb_res
 
 
 def get_args():
@@ -51,16 +51,11 @@ if __name__ == "__main__":
 
     nsg = NopSlideGenerator(args.length)
     nop_slide = nsg.get_slide()
-    print(f"nop_slide: {nop_slide}")
 
-    bytes = bytes(args.payload, "utf-8")
-    print(f"bytes: {bytes}")
+    pi = PayloadInserter(nop_slide, args.payload, args.outfile)
+    res = pi.get_combined_res()
+    print(res)
 
-    stripped_bytes = args.payload.replace("\\x", "")
-    print(f"stripped_bytes: {stripped_bytes}")
-
-    # print(binascii.hexlify(hex))
-
-    # PayloadInserter(nop_slide, bytes.fromhex(hex))
-
-    # print(codecs.decode(str_payload, "hex_codec"))
+    if args.outfile:
+        with open(args.outfile, "w+") as f:
+            f.writelines(res)
